cd73d4745111b35ef1117af1f9f8caa1eb8bc31d
Yusuf <kurakiorg@gmail.com>
2023-03-20 17:01:14 +0300

    Day 9: Soung Board

---
+++ 9D/app.js
@@ -0,0 +1,20 @@
+const sounds = ["game-over", "notif"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+  document.querySelector(".buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
